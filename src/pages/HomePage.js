44ba0246a65d1f78fe8e8bc85d51b52a74d09d2f
Sam Slosser <samuel.slosser@rescuedogdesign.com>
2020-02-17 15:56:05 -0500

    at chp 1 section 6

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+    <h1>Hello, welcome to my blog!</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Metus aliquam eleifend mi in. Integer eget aliquet nibh praesent tristique magna sit amet purus. Enim nunc faucibus a pellentesque. Ullamcorper eget nulla facilisi etiam dignissim diam quis. Ac turpis egestas maecenas pharetra convallis posuere morbi leo. Phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet. Vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Nibh mauris cursus mattis molestie a iaculis at. Facilisis magna etiam tempor orci eu lobortis elementum nibh tellus. Egestas congue quisque egestas diam in arcu. Sed risus ultricies tristique nulla aliquet enim tortor.
+    </p>
+    <p>
+        Dictumst quisque sagittis purus sit amet volutpat. Risus at ultrices mi tempus. Diam in arcu cursus euismod quis. Cum sociis natoque penatibus et. Enim tortor at auctor urna nunc id cursus. Nibh nisl condimentum id venenatis a condimentum vitae sapien. Consequat ac felis donec et odio. Massa massa ultricies mi quis hendrerit dolor. Phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet enim. Est placerat in egestas erat imperdiet sed euismod nisi. Eget duis at tellus at urna condimentum. Diam donec adipiscing tristique risus nec feugiat. Erat imperdiet sed euismod nisi porta. Amet tellus cras adipiscing enim. Fermentum odio eu feugiat pretium. Convallis a cras semper auctor neque vitae. Amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus.
+    </p>
+    </>
+)
+
+export default HomePage;
